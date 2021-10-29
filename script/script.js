fd0ea322fc49016fe0aa360f5e3dc4da6a749b28
Carla Bisagni <carla.bisagni@accenture.com>
2021-10-28 23:03:16 -0300

    last upd

--- script/script.js
+++ script/script.js
@@ -12,3 +12,40 @@ function openMenu() {
 function closeMenu(){
     listaMenu.style.display = "none";
 }
+
+const btnOpenModal = document.querySelector("#ver-mas-modal");
+const modal = document.querySelector("#modal1");
+
+btnOpenModal.addEventListener("click", openModal);
+
+function openModal(){
+    modal.style.display = "grid";
+}
+
+window.onclick = function(e){
+    if(e.target == modal){
+        modal.style.display = "none";
+    }else if(e.target == modal2){
+        modal2.style.display = "none";
+    }else if(e.target == modal3){
+        modal3.style.display = "none";
+    }
+};
+
+const btnOpenModal2 = document.querySelector("#ver-mas-modal2");
+const modal2 = document.querySelector("#modal2");
+
+btnOpenModal2.addEventListener("click", openModal2);
+
+function openModal2(){
+    modal2.style.display = "grid";
+}
+
+const btnOpenModal3 = document.querySelector("#ver-mas-modal3");
+const modal3 = document.querySelector("#modal3");
+
+btnOpenModal3.addEventListener("click", openModal3);
+
+function openModal3(){
+    modal3.style.display = "grid";
+}
