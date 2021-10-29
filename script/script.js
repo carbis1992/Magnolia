0b15ee2f79da196fb875a7442fae3714d722c8f8
Carla Bisagni <carla.bisagni@accenture.com>
2021-10-29 15:49:22 -0300

    finish responsive

--- script/script.js
+++ script/script.js
@@ -15,11 +15,17 @@ function closeMenu(){
 
 const btnOpenModal = document.querySelector("#ver-mas-modal");
 const modal = document.querySelector("#modal1");
+const btnClose = document.querySelector("#closeModal1");
 
 btnOpenModal.addEventListener("click", openModal);
+btnClose.addEventListener("click", closeModal1);
 
 function openModal(){
     modal.style.display = "grid";
+}
+
+function closeModal1(){
+    modal.style.display = "none";
 }
 
 window.onclick = function(e){
@@ -34,18 +40,33 @@ window.onclick = function(e){
 
 const btnOpenModal2 = document.querySelector("#ver-mas-modal2");
 const modal2 = document.querySelector("#modal2");
+const btnClose2 = document.querySelector("#closeModal2");
 
 btnOpenModal2.addEventListener("click", openModal2);
+btnClose2.addEventListener("click", closeModal2);
 
 function openModal2(){
     modal2.style.display = "grid";
 }
 
+function closeModal2(){
+    modal2.style.display = "none";
+}
+
 const btnOpenModal3 = document.querySelector("#ver-mas-modal3");
 const modal3 = document.querySelector("#modal3");
+const btnClose3 = document.querySelector("#closeModal3");
 
 btnOpenModal3.addEventListener("click", openModal3);
+btnClose3.addEventListener("click", closeModal3);
 
 function openModal3(){
     modal3.style.display = "grid";
 }
+
+function closeModal3(){
+    modal3.style.display = "none";
+}
+
+var myIdToInsert = 1;
+document.getElementById("btn1").href += myidtoinsert;
